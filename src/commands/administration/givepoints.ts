457f4e4f3d2cc45abc7fee72be2d943c83687903
Dep0sit <ashkarmalek0@gmail.com>
2020-11-20 08:03:13 -0500

    :sparkles: Give and Take command

---
+++ src/commands/administration/givepoints.ts
@@ -0,0 +1,45 @@
+import { Message } from "discord.js";
+import AdminCommand from ".";
+import { UserModel } from "../../models/user";
+import embeds from "../../utils/embeds";
+
+export default class GivePointsCommand extends AdminCommand {
+  cmdName = "givepoints";
+  description = "Give points to a player.";
+
+  async run(message: Message, args: string[]) {
+    const user = message.mentions.users.first();
+    if (!user)
+      return message.channel.send(
+        embeds.error(`Please tag the user you would like to give points to!`)
+      );
+
+    const points = !isNaN(parseInt(args[1])) ? parseInt(args[1]) : null;
+    if (!points)
+      return message.channel.send(
+        embeds.error(
+          `Please provide the amount of points you would like to give ${user}.`
+        )
+      );
+
+    const userData =
+      (await UserModel.findOne({ userId: user.id })) ||
+      (await UserModel.create({ userId: user.id }));
+
+    await UserModel.updateOne(
+      {
+        _id: userData._id,
+      },
+      {
+        $inc: { points },
+      }
+    );
+
+    return await message.channel.send(
+      embeds.normal(
+        `Points Given`,
+        `${message.author} has given ${user} **${points} points**.`
+      )
+    );
+  }
+}
---
+++ src/commands/administration/takepoints.ts
@@ -0,0 +1,52 @@
+import { Message } from "discord.js";
+import AdminCommand from ".";
+import { UserModel } from "../../models/user";
+import embeds from "../../utils/embeds";
+
+export default class TakePointsCommand extends AdminCommand {
+  cmdName = "takepoints";
+  description = "Take points from a player.";
+
+  async run(message: Message, args: string[]) {
+    const user = message.mentions.users.first();
+    if (!user)
+      return message.channel.send(
+        embeds.error(`Please tag the user you would like to take points from!`)
+      );
+
+    const points = !isNaN(parseInt(args[1])) ? parseInt(args[1]) : null;
+    if (!points)
+      return message.channel.send(
+        embeds.error(
+          `Please provide the amount of points you would like to take from ${user}.`
+        )
+      );
+
+    const userData =
+      (await UserModel.findOne({ userId: user.id })) ||
+      (await UserModel.create({ userId: user.id }));
+
+    if (userData.points < points)
+      return message.channel.send(
+        embeds.error(
+          `${user} only has **${userData.points} points**. You may not take **${points} points** from them!`
+        )
+      );
+
+    await UserModel.updateOne(
+      {
+        _id: userData._id,
+      },
+      {
+        $inc: { points: -points },
+      }
+    );
+
+    return await message.channel.send(
+      embeds.normal(
+        `Points Taken`,
+        `${message.author} has taken **${points} points** from ${user}.`
+      )
+    );
+  }
+}
