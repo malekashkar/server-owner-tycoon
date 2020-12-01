878f19cb13b1bb24c3a149c0afae231a17766c32
Dep0sit <ashkarmalek0@gmail.com>
2020-12-01 14:11:40 -0500

    :sparkles: Confirmation when entering a giveaway

--- src/events/games/giveawayAnswers.ts
+++ src/events/games/giveawayAnswers.ts
@@ -1,6 +1,8 @@
 import { DMChannel, Message } from "discord.js";
 import Event, { EventNameType } from "..";
 import { GiveawayModel } from "../../models/giveaway";
+import confirmation from "../../utils/confirmation";
+import embeds from "../../utils/embeds";
 
 export default class GiveawayAnswers extends Event {
   name: EventNameType = "message";
@@ -14,12 +16,25 @@ export default class GiveawayAnswers extends Event {
         endsAt: { $gte: new Date() },
         ended: false,
       });
-      if (currentGiveaway.winners?.includes(message.author.id)) return;
-      if (!currentGiveaway.participants?.includes(message.author.id))
-        currentGiveaway.participants.push(message.author.id);
-      if (parseInt(message.content) === currentGiveaway.randomNumber)
-        currentGiveaway.winners.push(message.author.id);
-      await currentGiveaway.save();
+      if (!currentGiveaway.participants?.includes(message.author.id)) {
+        const guessedNumber = parseInt(message.content);
+        if (guessedNumber < 1 || guessedNumber > 100) {
+          message.channel.send(
+            embeds.error(`Please provide a number from 1-100.`)
+          );
+        } else {
+          const confirm = await confirmation(
+            `Number Choice Confirmation`,
+            `Are you sure you would like to choose the number ${guessedNumber}?`
+          );
+          if (!confirm) return;
+
+          currentGiveaway.participants.push(message.author.id);
+          if (guessedNumber === currentGiveaway.randomNumber)
+            currentGiveaway.winners.push(message.author.id);
+          await currentGiveaway.save();
+        }
+      }
     }
   }
 }
